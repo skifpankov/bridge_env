e0bae574c8aeaa3ab8582a49a9a3fba890a81fe6
skifpankov <skif.pankov.89@gmail.com>
2019-07-28 14:36:38 +0100

    Changes in config: suits are now in ascending order

--- config.py
+++ config.py
@@ -20,13 +20,18 @@ FULL_DECK = list(range(NUM_CARDS))
 Seat2Group = {0: 0, 1: 1, 2: 0, 3: 1} # or s%2
 
 Seat2str = {0: "N", 1: "E", 2: "S", 3: "W"}
-Strain2str = {0: "S", 1: "H", 2: "D", 3: "C", 4: "N"}
-Suit2str = {0: "S", 1: "H", 2: "D", 3: "C"}
+
+# Suit2str is defined in ascending order of Trumps, i.e. Clubs, Diamonds, Hears, Spades (C,D,H,S)
+Suit2str = {0: "C", 1: "D", 2: "H", 3: "S"}
+
+# Strain2str is created by merging Suit2str with an extra element for "No Trump" (N)
+Strain2str = {**Suit2str, **{4: "N"}}
+
 Rank2str = {2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'T', 11: 'J', 12: 'Q', 13: 'K', 14: 'A'}
 
 # Convenient for parallel processing
-# ScoreScale = {0: 30, 1: 30, 2: 20, 3: 20, 4: 30}
-ScoreScale = np.array([30, 30, 20, 20, 30])
+# ScoreScale = {0: 20, 1: 20, 2: 30, 3: 30, 4: 30}
+ScoreScale = np.array([20, 20, 30, 30, 30])
 # ScoreBias = {0: 0, 1: 0, 2: 0, 3: 0, 4: 10}
 ScoreBias = np.array([0, 0, 0, 0, 10])
 
